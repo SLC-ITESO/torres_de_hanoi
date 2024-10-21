8ce040d9ab13dd5493224fad02091baf8deb79da
Santiago Ismael López Contreras <74425838+SLC-ITESO@users.noreply.github.com>
2024-10-20 22:32:18 -0600

    Update Torres_Hanoi.asm
    Falta el ultimo scramble para que funcione la lógica de intercambio de torres.

--- Torres_Hanoi.asm
+++ Torres_Hanoi.asm
@@ -3,8 +3,8 @@
 # Impares 1 3 2
 # Pares 1 2 3
 addi s0, zero, 3				# n = x (cantidad de Discos)
-lui a0, 0x10010					# a0: direcciÃ³n de la primera torre (origen)
-add a7, zero, a0				# se guarda la direcciÃ³n del disco
+lui a0, 0x10010					# a0: direccion de la primera torre (origen)
+add a7, zero, a0				# se guarda la direccion del disco
 
 addi t0, zero, 1				# Para comparar si n = 1
 jal hanoi
@@ -22,39 +22,57 @@ inicia_torre:
 termina_torre:
 	addi s0, s0, -1				# restauramos n
 	addi a0, a0, -0x20			
-	
 	addi a1, a0, 0x4			# a1: direccion segunda torre (auxiliar)
 	#sw t6 0x0(a1)
 	addi a2, a1, 0x4			# a2: direccion tercera torre (destino)
 	#sw t6 0x0(a2)
-	
 	# LAS DIRECCIONES SE ENCUENTRAN EN LA "BASE" DE LAS TORRES
+	
+	#Para mejor entendimiento, se le asignara un numero a cada torre
+	addi t3, zero, 1			# 1 sera la torre A, o torre ORIGEN
+	addi t4, zero, 2			# 2 sera la torre B, o torre AUXILIAR
+	addi t5, zero, 3			# 3 sera la torre C, o torre DESTINO
+	
 	jal movedera_recursiva
 	#void hanoi(int n, char origen, char destino, char auxiliar)
-	#void hanoi( s0, a0, a2, a1)
+	#
 	jal si_termina
 	
 movedera_recursiva: #void hanoi()
 	
 	beq s0, t0, es_uno			# if n == 1
 	
-	#hanoi(n - 1, origen, auxiliar, destino);
-    	addi sp, sp, -0x8			#si se necesita más, aumentale
+    	addi sp, sp, -0x14
     	sw ra, 0x0(sp)
     	sw s0, 0x4(sp)
+    	sw t3, 0x8(sp)				# origen
+    	sw t5, 0xC(sp)				# destino
+    	sw t4, 0x10(sp)				# auxiliar
+    	
+    	#hanoi(n - 1, origen, auxiliar, destino);
     	addi s0, s0, -1
+    	#-
+	add t2 t4, zero  			# t2 = t4 | almacenamos t4 en t2
+	add t4, t5, zero  			# t4 = t5
+	add t5, t2, zero  			# t5 = t2 | t2 tenía el valor original de t4
     	
     	jal movedera_recursiva
     	#printf("Mover disco %d de %c a %c\n", n, origen, destino);
 
     	lw ra, 0x0(sp)
     	lw s0, 0x4(sp)
-    	addi sp, sp, 0x8			#si se necesita más, aumentale
-	addi s0, s0, -1
+    	lw t3, 0x8(sp)
+    	lw t5, 0xC(sp)
+    	lw t4, 0x10(sp)
+    	addi sp, sp, 0x14
 	
 	#hanoi(n - 1, auxiliar, destino, origen);
+	addi s0, s0, -1
+	#Falta revisar esta logica!
+	#add t2, t3, zero         # t2 = origen (almacenamos el origen en t2)
+	#add t3, t5, zero         # t3 = destino (almacenamos el destino en t3)
+	#add t5, t4, zero         # t5 = auxiliar (el auxiliar pasa a ser el destino)
 	jal movedera_recursiva
-	lw ra,0x0(sp)
 	
 	jalr ra
 	
